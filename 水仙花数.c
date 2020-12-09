83ea6d2a72569e02911217817100825f1b6872a8
lijilong5201314 <2413045612@qq.com>
2020-12-09 23:40:12 +0800

    水仙花

---
+++ 水仙花数.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int hun, ten, ind, n;
+	printf("result is:");
+	for (n = 100; n<1000; n++)  /*整数的取值范围*/
+	{
+		hun = n / 100;
+		ten = (n - hun * 100) / 10;
+		ind = n % 10;
+		if (n == hun*hun*hun + ten*ten*ten + ind*ind*ind)  /*各位上的立方和是否与原数n相等*/
+			printf("%d  ", n);
+	}
+	printf("\n");
+
+	return 0;
+}
