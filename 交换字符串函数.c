e6fbd3fd3bc254af1f9ea08d0ce0bd34e022c685
lov1ballad <74500693+lov1ballad@users.noreply.github.com>
2020-12-16 11:17:53 +0800

    Update 交换字符串函数.c

--- 交换字符串函数.c
+++ 交换字符串函数.c
@@ -1,1 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <assert.h>
+#include<stdio.h>
+char* my_strcpy(char* dest, const char*src)
+{
+	char* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+
+	while (*dest++ = *src++)
+	{
+		;
+	}
+
+	return ret;
+}
+
+int main()
+{
+	char arr1[20] = "xxxxxxxx";
+	char *p = "abcdef";
+	my_strcpy(arr1, p);
+	printf("%s\n", arr1);
+
+	return 0;
+}
